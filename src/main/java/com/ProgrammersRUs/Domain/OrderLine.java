5e30ee1b17dc2f6fb12bf3ffe0f9556d3c757c55
MichaelDJansen <michaeldjansen35@gmail.com>
2015-10-16 06:13:38 +0200

    Completed OrderLine

--- src/main/java/com/ProgrammersRUs/Domain/OrderLine.java
+++ src/main/java/com/ProgrammersRUs/Domain/OrderLine.java
@@ -10,18 +10,55 @@ public class OrderLine {
     private Long itemId;
     private int quantity;
 
-     /*
-    public Supplier(Builder builder){}
+
+    public OrderLine(Builder builder){
+        this.orderId = builder.orderId;
+        this.itemId = builder.itemId;
+        this.quantity = builder.quantity;
+    }
 
     public class Builder{
 
+        private Long id;
+        private Long orderId;
+        private Long itemId;
+        private int quantity;
         public Builder(){}
 
-        public Builder copy(){}
+        public Builder(Long orderId,Long itemId,int quantity){
+            this.orderId = orderId;
+            this.itemId = itemId;
+            this.quantity = quantity;
+        }
 
-        public Supplier build(){}
+        public Builder orderId(Long orderId){
+            this.orderId = orderId;
+            return this;
+        }
+
+        public Builder itemId(Long itemId){
+            this.itemId = itemId;
+            return this;
+        }
+
+        public Builder quantity(int quantity){
+            this.quantity = quantity;
+            return this;
+        }
+
+        public Builder copy(OrderLine orderLine){
+            this.id = orderLine.id;
+            this.orderId = orderLine.orderId;
+            this.itemId = orderLine.itemId;
+            this.quantity = orderLine.quantity;
+            return this;
+        }
+
+        public OrderLine build(Builder builder){
+            return new OrderLine(builder);
+        }
     }
-    */
+
 
     public Long getId() {
         return id;
