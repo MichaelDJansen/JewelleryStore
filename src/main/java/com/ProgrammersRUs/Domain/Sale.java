1a4529a3edf895c235423fc8e955543497223eb7
MichaelDJansen <michaeldjansen35@gmail.com>
2015-10-16 06:18:55 +0200

    Completed OrderLine

--- src/main/java/com/ProgrammersRUs/Domain/Sale.java
+++ src/main/java/com/ProgrammersRUs/Domain/Sale.java
@@ -10,18 +10,38 @@ public class Sale {
     private Long transction_number;
     private Long orderId;
 
-     /*
-    public Supplier(Builder builder){}
+
+    public Sale(Builder builder){
+        this.transction_number = builder.transction_number;
+        this.orderId = builder.orderId;
+    }
 
     public class Builder{
 
+        private Long transction_number;
+        private Long orderId;
+
         public Builder(){}
 
-        public Builder copy(){}
+        public Builder(Long orderId){
+            this.orderId = orderId;
+        }
+        public Builder orderId(Long orderId){
+            this.orderId = orderId;
+            return this;
+        }
 
-        public Supplier build(){}
+        public Builder copy(Sale sale){
+            this.transction_number = sale.transction_number;
+            this.orderId = sale.orderId;
+            return this;
+        }
+
+        public Sale build(Builder builder){
+            return new Sale(builder);
+        }
     }
-    */
+
 
 
     public Long getTransction_number() {
