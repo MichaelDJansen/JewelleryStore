b1522d2305b6038d3b5d93f567000b93a95a42f3
MichaelDJansen <michaeldjansen35@gmail.com>
2015-10-16 06:25:33 +0200

    Completed Supplier

--- src/main/java/com/ProgrammersRUs/Domain/Supplier.java
+++ src/main/java/com/ProgrammersRUs/Domain/Supplier.java
@@ -6,23 +6,57 @@ package com.ProgrammersRUs.Domain;
 public class Supplier {
     private Long id;
     private String supplierName;
-    private int itemId;
+    private Long itemId;
     private String type;
 
     private Supplier(){}
 
-    /*
+
     public Supplier(Builder builder){}
 
     public class Builder{
 
+        private Long id;
+        private String supplierName;
+        private Long itemId;
+        private String type;
+
         public Builder(){}
 
-        public Builder copy(){}
+        public Builder(String supplierName,Long itemId,String type){
+            this.supplierName = supplierName;
+            this.itemId = itemId;
+            this.type = type;
+        }
 
-        public Supplier build(){}
+        public Builder supplierName(String supplierName){
+            this.supplierName = supplierName;
+            return this;
+        }
+
+        public Builder itemId(Long itemId){
+            this.itemId = itemId;
+            return this;
+        }
+
+        public Builder type(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public Builder copy(Supplier supplier){
+            this.id = supplier.id;
+            this.supplierName = supplier.supplierName;
+            this.itemId = supplier.itemId;
+            this.type = supplier.type;
+            return this;
+        }
+
+        public Supplier build(Builder builder){
+            return new Supplier(builder);
+        }
     }
-    */
+
 
 
     public Long getId() {
@@ -33,7 +67,7 @@ public class Supplier {
         return supplierName;
     }
 
-    public int getItemId() {
+    public Long getItemId() {
         return itemId;
     }
 
