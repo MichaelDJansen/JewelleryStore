3ed94357a4afec2b370cdf5f4f262518dff20a38
Yusra94 <yusraismail17@gmail.com>
2015-10-29 22:19:12 +0000

    adding customer resource

---
+++ src/main/java/com/ProgrammersRUs/Model/CustomerResource.java
@@ -0,0 +1,90 @@
+package com.ProgrammersRUs.Model;
+
+import com.ProgrammersRUs.Domain.Address;
+import com.ProgrammersRUs.Domain.ContactInformation;
+import com.ProgrammersRUs.Domain.Name;
+import org.springframework.hateoas.ResourceSupport;
+
+/**
+ * Created by yusraAdmin on 10/29/2015.
+ */
+public class CustomerResource extends ResourceSupport {
+
+    private Long id;
+    private Name name;
+    private ContactInformation contactInformation;
+    private Address address;
+
+    protected CustomerResource(){}
+
+    public CustomerResource(Builder builder){
+        this.id = builder.id;
+        this.name = builder.name;
+        this.contactInformation = builder.contactInformation;
+        this.address = builder.address;
+    }
+
+    public static class Builder{
+        private Long id;
+        private Name name;
+        private ContactInformation contactInformation;
+        private Address address;
+
+        public Builder(){
+        }
+
+        public Builder(Name name,ContactInformation contactInformation){
+            this.name = name;
+            this.contactInformation = contactInformation;
+        }
+
+        public Builder name(Name name){
+            this.name = name;
+            return this;
+        }
+
+        public Builder contactInformation(ContactInformation contactInformation){
+            this.contactInformation = contactInformation;
+            return this;
+        }
+
+        public Builder address(Address address){
+            this.address = address;
+            return this;
+        }
+
+        public Builder id(Long id){
+            this.id = id;
+            return this;
+        }
+
+        public Builder copy(CustomerResource customer){
+            this.id = customer.id;
+            this.name = customer.name;
+            this.contactInformation = customer.contactInformation;
+            this.address = customer.address;
+            return this;
+        }
+
+        public CustomerResource build(){
+            return new CustomerResource(this);
+        }
+    }
+
+    public Long getCustomerId() {
+        return id;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public ContactInformation getContactInformation() {
+        return contactInformation;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+}
